a7a4a313d00aaaaa70dd0d0dc10c853c8e59f4ff
themdshakibul <mdshakibul88240@gmail.com>
2026-06-08 08:54:14 +0600

    add: mongodb to connect successfully

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 dotenv.config();
 
 const port = process.env.PORT;
-const url = process.env.MONGO_URI;
+const uri = process.env.MONGODB_URI;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Create a MongoClient
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // await client.connect();
+    // await client.db("admin").command({ ping: 1 });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Wellcome ticketo-server!");
